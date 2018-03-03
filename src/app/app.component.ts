f5c7398dea241848dc46f91123c553c8c84a4f87
Dudás Áron <aron@chrome.hu>
2018-03-03 23:00:23 +0100

    adding three js for the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+  }
+
+  public startRendering(): void {
+
+    // Get the DOM element to attach to
+    const container = document.getElementById('preview-canvas');
+
+    // Set the scene size.
+    const WIDTH = container.clientWidth;
+    const HEIGHT = container.clientHeight;
+
+    // Set some camera attributes.
+    const VIEW_ANGLE = 45;
+    const ASPECT = WIDTH / HEIGHT;
+    const NEAR = 0.1;
+    const FAR = 10000;
+
+
+    // Create a WebGL renderer, camera
+    // and a scene
+    const renderer = new THREE.WebGLRenderer();
+    const camera =
+      new THREE.PerspectiveCamera(
+        VIEW_ANGLE,
+        ASPECT,
+        NEAR,
+        FAR
+      );
+
+    const scene = new THREE.Scene();
+
+    // Add the camera to the scene.
+    scene.add(camera);
+
+    // Set up the sphere vars
+    const RADIUS = 50;
+    const SEGMENTS = 16;
+    const RINGS = 16;
+
+    // create the sphere's material
+    const sphereMaterial =
+    new THREE.MeshLambertMaterial(
+      {
+        color: 0xCC0000
+      });
+
+    // Create a new mesh with
+    // sphere geometry - we will cover
+    // the sphereMaterial next!
+    const sphere = new THREE.Mesh(
+
+      new THREE.SphereGeometry(
+        RADIUS,
+        SEGMENTS,
+        RINGS),
+
+      sphereMaterial);
+
+    // Move the Sphere back in Z so we
+    // can see it.
+    sphere.position.z = -300;
+
+    // Finally, add the sphere to the scene.
+    scene.add(sphere);
+
+    // create a point light
+    const pointLight =
+    new THREE.PointLight(0xFFFFFF);
+
+    // set its position
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+
+    // add to the scene
+    scene.add(pointLight);
+
+    // Start the renderer.
+    renderer.setSize(WIDTH, HEIGHT);
+
+    renderer.render(scene, camera);
+
+    $(container).empty();
+    // Attach the renderer-supplied
+    // DOM element.
+    container.appendChild(renderer.domElement);
+  }
 }
